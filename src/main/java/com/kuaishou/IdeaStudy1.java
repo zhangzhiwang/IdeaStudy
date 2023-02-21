8ce4de7b3364c32736425053aa550ff6586d89c9
zzw1 <934109401@qq.com>
2023-02-21 22:58:47 +0800

    新建项目

--- src/main/java/com/kuaishou/IdeaStudy1.java
+++ src/main/java/com/kuaishou/IdeaStudy1.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IdeaStudy1 extends  IdeaStudy1Father {
+    /**
+     *
+     */
     private int age;
 
     public IdeaStudy1(int age) {
