441424877e7bde67110301a963e1ee332aae883b
zhangzhiwang <wb_zhangzhiwang@kuaishou.com>
2023-02-25 21:44:10 +0800

    变更2

--- IdeaStudySubModule_1/src/main/java/com/kuaishou/App.java
+++ IdeaStudySubModule_1/src/main/java/com/kuaishou/App.java
@@ -6,5 +6,8 @@ package com.kuaishou;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println(1);
+        System.out.println(2);
     }
 }
