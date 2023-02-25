4d7ba6437678d182c6414eda1ed55f041a3dfb49
zhangzhiwang <wb_zhangzhiwang@kuaishou.com>
2023-02-25 21:41:45 +0800

    1

--- IdeaStudySubModule_1/src/main/java/com/kuaishou/App.java
+++ IdeaStudySubModule_1/src/main/java/com/kuaishou/App.java
@@ -3,10 +3,8 @@ package com.kuaishou;
 /**
  * Hello world!
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
