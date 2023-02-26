6bc9ca85c03bb2c0b83e5740417ffdd9a5839794
zhangzhiwang <wb_zhangzhiwang@kuaishou.com>
2023-02-26 14:31:47 +0800

    zzw

--- IdeaStudySubModule_1/src/main/java/com/kuaishou/App.java
+++ IdeaStudySubModule_1/src/main/java/com/kuaishou/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        System.out.println(1);
+        System.out.println(1 + "zzw");
         System.out.println(2);
         System.out.println(333);
     }
