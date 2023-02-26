394b30a8e6794c5068200af9468d511c771ba42b
zhangzhiwang <wb_zhangzhiwang@kuaishou.com>
2023-02-26 16:07:09 +0800

    z:x=2

--- src/main/java/com/kuaishou/IdeaStudy1.java
+++ src/main/java/com/kuaishou/IdeaStudy1.java
@@ -50,6 +50,7 @@ public class IdeaStudy1 extends  IdeaStudy1Father {
 //        Thread t = new Thread(() -> {
 //            System.out.println(123);
 //        });
+        int x = 2;
     }
 
     public static int f1(String s) {
