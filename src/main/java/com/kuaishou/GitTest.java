c4cab82f0005e93b03f54e8b15c1d15a20b2c6f2
zhangzhiwang <wb_zhangzhiwang@kuaishou.com>
2023-02-26 21:43:16 +0800

    s2

--- src/main/java/com/kuaishou/GitTest.java
+++ src/main/java/com/kuaishou/GitTest.java
@@ -3,7 +3,7 @@ package com.kuaishou;
 public class GitTest {
     private int i = 52;
 
-    public void m1(String s1) {}
+    public void m1(String s1, String s2) {}
 
     public void m2(int ii) {}
 
