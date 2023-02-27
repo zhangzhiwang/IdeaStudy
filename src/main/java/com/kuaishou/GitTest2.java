a8d9ed89019ee1db4c0d82fe3c6e2436d491da09
zzw1 <934109401@qq.com>
2023-02-27 23:08:34 +0800

    z:del m1 s3

--- src/main/java/com/kuaishou/GitTest2.java
+++ src/main/java/com/kuaishou/GitTest2.java
@@ -5,18 +5,18 @@ package com.kuaishou;
  * @Date 2023/2/27 21:30
  */
 public class GitTest2 {
-    private int i = 4;
+    private int i = 400;
 
-    public void m1(String s1, String s2, String s3) {
-        System.out.println("master m1 1");
+    public void m1(String s1, String s2) {
+
     }
 
     public void m2(int ii, int i2, int i3) {
-        System.out.println("master m2 1");
+
     }
 
     public void m3(boolean b, boolean b2, boolean b3) {
-        System.out.println("master m3 1");
+
     }
 }
 
