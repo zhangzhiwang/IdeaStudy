0bf0fd8a0c14a25081c91c4bb8e342f00ebb6a61
zzw1 <934109401@qq.com>
2023-02-27 22:04:37 +0800

    master:m1 1

--- src/main/java/com/kuaishou/GitTest2.java
+++ src/main/java/com/kuaishou/GitTest2.java
@@ -8,7 +8,7 @@ public class GitTest2 {
     private int i = 2;
 
     public void m1(String s1, String s2, String s3) {
-
+        System.out.println("master m1 1");
     }
 
     public void m2(int ii, int i2, int i3) {
